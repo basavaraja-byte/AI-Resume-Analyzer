7ee29251673cececae18bf11e0b631765082c5f9
Basavaraja Beerabbi <bbeerabbi@gmail.com>
2026-06-19 12:45:23 +0530

    Fixed Streamlit deployment error

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from resume_parser import extract_text_from_pdf
 from analyzer import detect_skills
-from resume_parser import extract_text_from_pdf
 
 st.set_page_config(page_title="AI Resume Analyzer")
 
@@ -17,16 +16,19 @@ if uploaded_file:
     st.success("Resume Uploaded Successfully")
 
     text = extract_text_from_pdf(uploaded_file)
-skills = detect_skills(text)
 
-st.subheader("Detected Skills")
+    skills = detect_skills(text)
 
-for skill in skills:
-    st.success(skill)
+    st.subheader("Detected Skills")
+
+    for skill in skills:
+        st.success(skill)
+
     st.subheader("Resume Content")
 
     st.text_area(
         "Extracted Text",
         text,
-        height=400
+        height=400,
+        key="resume_text"
     )
